797795fb46a459bdc2ff31a80933880b5202ce6a
Ahmad Fauzan <ahmadfauzan190603@gmail.com>
2022-10-24 11:18:43 +0700

    add Home Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from './components/Home'
 import MeetTheTeam from './components/MeetTheTeam'
 import OurTeam from './components/OurTeam'
 
 function App() {
   return (
     <div className='overflow-x-hidden'>
+      <Home />
       <MeetTheTeam />
       <OurTeam />
       {/* manggilnya di sini pak */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import logoNL from '../images/Logo_NL.svg'
+import titleNL from '../images/Title_NL.svg'
+
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex h-[100vh] items-center justify-center'>
+        <div className='w-[50%] h-full flex justify-center'>
+          <img src={logoNL} className='w-[60%]' />
+        </div>
+
+        <div className='w-[50%] pr-[180px]'>
+          <img  src={titleNL} className='w-[70%]' />
+          <h1 className='mb-[24px] mt-[-10px] text-[40px]'>DTETI FT UGM</h1>
+          <p>A Community in Electrical and Information Engineering
+            Department Gadjah Mada University that accomodates 
+            student's to develop their skills, creativity, and
+            innovation in the <span className='text-[#C42439]'> IT field </span></p>
+        </div>
+      </div>
+    </>
   )
 }
